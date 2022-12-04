fc9c04976108e1e533cd2688bc368eac4c17c4bd
kiricord <rikudosennin410@gmail.com>
2022-12-04 17:16:02 +0500

    jupiter

--- labs/white.py
+++ labs/white.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 # среднее число электронов
 Ye = 26 / 56
-rho0 = 9.79 * 10e5 * (1 / Ye)  # г * (см ** -3)
-m0 = 5.67 * 10e33 * (Ye ** 2)  # г
-r0 = 7.72 * 10e8 * Ye  # см
+rho0 = 9.79e5 * (1 / Ye)  # г * (см ** -3)
+m0 = 5.67e33 * (Ye ** 2)  # г
+r0 = 7.72e8 * Ye  # см
 # rho0 = 1
 
 
@@ -26,49 +26,52 @@ def odes(r, y):
 
 
 def whiteDwarf(rho_c, method='RK45'):
-    masses = []
-    radi = []
-    densities = []
-
+    solutions = []
     for i in range(len(rho_c)):
         rho_temp = rho_c[i]  # / rho0
-
-        rSpan = [3e-10, 10]
-
+        rSpan = [3e-14, 10]
         initCond = [rho_temp, 0]
+        def rho_f(r, y): return y[0] - 5.13e-17
+        rho_f.terminal = True
+        sol = solve_ivp(odes, t_span=rSpan, y0=initCond,
+                        method=method, events=rho_f)
+        # sol.t - радиус
+        # sol.y[0] - плотность
+        # sol.y[1] - масса
 
-        sol = solve_ivp(odes, t_span=rSpan, y0=initCond, method=method)
+        rhoValues = sol.y[0] * rho0
+        mValues = sol.y[1] * m0
+        rValues = sol.t * r0
 
-        # rhoValues = sol.y[0] * rho0
-        # mValues = sol.y[1] * m0
-        # rValues = sol.t * r0
+        solutions.append((rValues[-1], mValues[-1], rhoValues[-1]))
 
-        masses.append(sol.y[1][-1])
-        densities.append(sol.y[0][-1])
-        radi.append(sol.t[-1])
-
-    return radi, masses, densities
+    return solutions
 
 
 def plotSolution(solutions):
-    for radius, mass, densities in solutions:
-        plt.plot(radius, mass)
+    # for radius, mass, densities in solutions:
+    #     plt.plot(mass, radius)
+
+    plt.plot([solution[1] for solution in solutions], [solution[0]
+             for solution in solutions])
+
+    plt.axvline(solutions1[-1][1], linestyle="--",
+                color="black", label="Предел Чандрасекара")
+    plt.legend(fontsize=14)
+    plt.ylabel("Радиус")
+    plt.xlabel("Масса")
+    plt.title("Зависимость масса—радиус для белых карликов")
 
 
-ran = np.logspace(-3, 5, num=7)
+# ran = [10e-3, 10e-2, 10e-1, 10e1, 10e2, 10e3, 10e4]
+ran = np.logspace(-3, 5, num=30)
+
 solutions1 = whiteDwarf(ran)
 
 # print(len(solutions1[0][0]))
-# plotSolution(solutions1)
-print(solutions1)
-plt.plot(solutions1[1], solutions1[0])
+plotSolution(solutions1)
+# print(solutions1)
+# plt.plot(solutions1[1], solutions1[0])
 
 
-plt.axvline(solutions1[1][-1], linestyle="--",
-            color="black", label="Предел Чандрасекара")
-plt.legend(fontsize=14)
-
-plt.ylabel("Радиус")
-plt.xlabel("Масса")
-plt.title("Зависимость масса—радиус для белых карликов")
 plt.show()
